ec98daa8a87333fda62586576370d4271bf8d331
Ali Mohammadi <alimohammadi02321@gmail.com>
2024-12-20 21:34:53 +0330

    Improve/ get user input value

--- investment_calculator.go
+++ investment_calculator.go
@@ -12,6 +12,8 @@ func main() {
 	expectedReturnRate := 5.5
 	// var years float64 = 10
 
+	fmt.Scan(&investmentAmount)
+
 	futureValue := investmentAmount * math.Pow((1+expectedReturnRate/100), years)
 
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
