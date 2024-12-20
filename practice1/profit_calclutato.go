7b6c4fe9824eca014ac383ad18db285085362d90
Ali Mohammadi <alimohammadi02321@gmail.com>
2024-12-20 22:55:16 +0330

    Improve/ calculate revenue, expenses and ebt

--- practice1/profit_calclutato.go
+++ practice1/profit_calclutato.go
@@ -3,16 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	var revenue int
-	var expenses int
+	var revenue float64
+	var expenses float64
 	var taxRate float64
 
 	fmt.Print("Revenue: ")
 	fmt.Scan(&revenue)
 
-	fmt.Print(("Expenses"))
+	fmt.Print(("Expenses: "))
 	fmt.Scan(&expenses)
 
 	fmt.Print("Tax Rate: ")
 	fmt.Scan(&taxRate)
+
+	ebt := revenue - expenses
+	profit := ebt * (1 - taxRate/100)
+	ratio := ebt / profit
+
+	fmt.Println(ebt)
+	fmt.Println(profit)
+	fmt.Println(ratio)
+
 }
