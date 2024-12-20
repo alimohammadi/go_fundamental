08de0928acacb139acbb201cb4c0943a4ecd9e07
Ali Mohammadi <alimohammadi02321@gmail.com>
2024-12-20 22:41:41 +0330

    Feature/ initialize profit calculator practice

---
+++ practice1/profit_calclutato.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var revenue int
+	var expenses int
+	var taxRate float64
+
+	fmt.Print("Revenue: ")
+	fmt.Scan(&revenue)
+
+	fmt.Print(("Expenses"))
+	fmt.Scan(&expenses)
+
+	fmt.Print("Tax Rate: ")
+	fmt.Scan(&taxRate)
+}
